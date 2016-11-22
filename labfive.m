38549e7e1e9ae526d8b34ef238e55460a860a6ff
Andreas Eliason <andreashg.eliasson@gmail.com>
2016-11-22 16:03:03 +0000

    calculate prediction errors for different basis functions

--- labfive.m
+++ labfive.m
@@ -32,7 +32,7 @@ sig = norm(Xtr(ceil(rand*Ntr),:) - Xtr(ceil(rand*Ntr),:));
 % 3 Perform K-means clustering to find centres for the basis functions. Use
 % K = Ntr / 10
 K = round(Ntr/10);
-% K = 15;
+% K = 100;
 [idx, C] = kmeans(Xtr, K);
 
 % 4 Construct the design matrix
@@ -66,6 +66,8 @@ axis([-2 3 -1 3]);
 xlabel('Target', 'FontSize', 14);
 ylabel('Prediction', 'FontSize', 14);
 
+print -depsc f5-1.eps;
+
 % 7 What does the model predict at each point in the test set?
 yhUnSeen = zeros(Ntr, 1);
 uts = zeros(1, K);
@@ -83,6 +85,88 @@ title('RBF Prediction on Unseen Data', 'FontSize', 16);
 axis([-2 3 -1 3]);
 xlabel('Target', 'FontSize', 14);
 ylabel('Prediction', 'FontSize', 14);
+
+print -depsc f5-2.eps;
+
+% How do the training and test errors compare?
+% training set errors
+tr_errors = zeros(Ntr, 1);
+
+for iter1=1:Ntr
+    tr_errors(iter1) = abs(ytr(iter1) - yh(iter1));
+end
+
+tr_errors_mean = mean(tr_errors);
+
+% test set errors
+ts_errors = zeros(Ntr, 1);
+
+for iter1=1:Ntr
+    ts_errors(iter1) = abs(yts(iter1) - yhUnSeen(iter1));
+end
+
+ts_errors_mean = mean(ts_errors);
+
+ks = 15:1:115;
+tr_prediction_errors_mean2 = zeros(101, 1);
+ts_prediction_errors_mean2 = zeros(101, 1);
+
+for iter2=1:101
+    [idx2, C2] = kmeans(Xtr, ks(iter2));
+    
+    A2 = ones(Ntr, ks(iter2));
+    for i=1:Ntr
+         for j=1:ks(iter2)
+            A2(i, j) = exp(-norm(Xtr(i,:) - C2(j,:)) / sig^2);
+         end
+    end
+    
+    % Solve for the paramaters (weights) using pseudo inverse
+    lambda2 = A2 \ ytr;
+    
+    % Evaluate test set prediction
+    B2 = ones(Ntr, ks(iter2));
+
+    for i=1:Ntr
+         for j=1:ks(iter2)
+            B2(i, j) = exp(-norm(Xts(i,:) - C2(j,:)) / sig^2);
+         end
+    end
+    
+    tr_predictions2 = A2 * lambda2;
+    ts_predictions2 = B2 * lambda2;
+    
+    tr_prediction_errors2 = zeros(Ntr, 1);
+    ts_prediction_errors2 = zeros(Ntr, 1);
+    
+    for iter33=1:Ntr
+       tr_prediction_errors2(iter33) = abs(ytr(iter33) - tr_predictions2(iter33));
+       ts_prediction_errors2(iter33) = abs(yts(iter33) - ts_predictions2(iter33));
+    end
+    
+    tr_prediction_errors_mean2(iter2) = mean(tr_prediction_errors2);
+    ts_prediction_errors_mean2(iter2) = mean(ts_prediction_errors2);
+end
+
+% Plot K vs training prediction errors
+figure(3),clf,
+plot(ks, tr_prediction_errors_mean2, 'b', 'LineWidth', 2),
+title('K vs training set prediction errors', 'FontSize', 16);
+% axis([0 120 0 0.3]);
+xlabel('Basis functions', 'FontSize', 14);
+ylabel('Mean Prediction Error', 'FontSize', 14);
+
+print -depsc f5-3.eps;
+
+% Plot K vs test prediction errors
+figure(4),clf,
+plot(ks, ts_prediction_errors_mean2, 'b', 'LineWidth', 2),
+title('K vs test set prediction errors', 'FontSize', 16);
+% axis([0 120 0 0.6]);
+xlabel('Basis functions', 'FontSize', 14);
+ylabel('Mean Prediction Error', 'FontSize', 14);
+
+print -depsc f5-4.eps;
 
 % 8 Compare your results with the linerar regression model for lab 4. Does
 % the use of a nonlinear model improve predictions?
@@ -151,7 +235,9 @@ for iter=1:20
 end
 
 % Plot boxplots
-figure(3),
+figure(5),
 boxplot([lg_prediction_error_means rbf_prediction_error_means], 'Labels', {'Linear', 'RBF'}),
 ylabel('Mean Squared Difference prediction error', 'FontSize', 14);
 title('Prediction errors: Linear vs RBF', 'FontSize', 14);
+
+print -depsc f5-5.eps;
