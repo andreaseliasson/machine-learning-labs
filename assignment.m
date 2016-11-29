5732b544aa53d127d149fc5585335bae55180197
Andreas Eliason <andreashg.eliasson@gmail.com>
2016-11-29 15:47:51 +0000

    draw posterior probabilities in 3D graph

--- assignment.m
+++ assignment.m
@@ -27,21 +27,18 @@ Pmax = max(max([P1 P2]));
 figure(1), clf, contour(xRange, yRange, P1./(P1+P2), [0 0.5*Pmax], 'LineWidth', 2);
 hold on;
 plot(m1(1), m1(2), 'b*', 'LineWidth', 4);
-contour(xRange, yRange, P2, [0.1*Pmax 0.5*Pmax 0.8*Pmax], 'LineWidth', 2);
 plot(m2(1), m2(2), 'r*', 'LineWidth', 4);
 
+% Show 100 samples drawn from each of the classes superposed on the same
+% graph
+N = 100;
+X1 = mvnrnd(m1, C1, N);
+X2 = mvnrnd(m2, C2, N);
 
+hold on;
+plot(X1(:, 1), X1(:, 2), 'bx', X2(:, 1), X2(:, 2), 'ro');
+grid on;
 
-
-% N = 100;
-% X1 = mvnrnd(m1, C1, N);
-% X2 = mvnrnd(m2, C2, N);
-% 
-% figure(1);
-% hold on;
-% plot(X1(:, 1), X1(:, 2), 'bx', X2(:, 1), X2(:, 2), 'ro');
-% grid on;
-% 
-% W1 = -0.5 * inv(C1);
-% w1 = inv(C1) * m1;
-% w10 = -0.5 * m1' * inv(C1) * m1 - 0.5 * log(det(C1));
+% Draw the posterior probability as a three dimensional graph
+figure(2)
+mesh(xRange, yRange, P1./(P1+P2));
